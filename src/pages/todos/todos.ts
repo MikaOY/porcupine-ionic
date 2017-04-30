f03ed6ec05a61d2b6c0380fe945f6fa6a1b673b8
omnikitty <omnikitty.powers@gmail.com>
2017-04-30 17:40:22 +0800

    Trashed www folder

---
+++ src/pages/todos/todos.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'todos-page',
+  templateUrl: 'todos.html'
+})
+export class TodosPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
