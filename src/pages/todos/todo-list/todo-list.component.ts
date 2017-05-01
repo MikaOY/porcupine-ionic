7fb74272d54be16a36feb78cedf0ed32e9be3fef
Mika Yeap <mikaomarr@gmail.com>
2017-05-01 18:38:10 +0800

    Added todos array into todo-list.component

--- src/pages/todos/todo-list/todo-list.component.ts
+++ src/pages/todos/todo-list/todo-list.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../../app/todo.service';
+
+import { Todo } from '../../../app/todo';
 
 @Component({
-  selector: 'todo-list',
-  templateUrl: 'todo-list.html',
+    selector: 'todo-list',
+    templateUrl: 'todo-list.html',
 })
-export class TodoList {
+export class TodoList implements OnInit {
     
+    todos: Todo[];
+
+    constructor(private todoService: TodoService){
+
+    }
+
+    ngOnInit(): void {
+        this.todoService.getTodos().then(todos => this.todos = todos);
+    }
 }
