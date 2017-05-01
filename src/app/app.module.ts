f43a1fc28e2779c69025d4c702733cc56b18d511
Mika Yeap <mikaomarr@gmail.com>
2017-05-01 17:56:10 +0800

    Updated todo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,7 @@ import { SettingsPage } from '../pages/settings/settings';
 import { TodosPage } from '../pages/todos/todos';
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { TodoItem } from '../todo-item/todo-item'; //not working?
-
+import { TodoList } from '../pages/todos/todo-list/todo-list'; 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TodoService } from './todo.service';
@@ -19,7 +18,7 @@ import { TodoService } from './todo.service';
     SettingsPage,
     TodosPage,
     TabsPage,
-    TodoItem
+    TodoList
   ],
   imports: [
     BrowserModule,
--- src/todo-item/todo-item.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'todo-item',
-  templateUrl: 'todo-item.html',
-})
-export class TodoItem {
-
-}
