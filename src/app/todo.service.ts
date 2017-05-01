559e885a55d70bd6689255cc8c2e0a28213bab93
Mika Yeap <mikaomarr@gmail.com>
2017-05-01 10:07:19 +0800

    Updated data classes + service

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 
 import { Todo } from './todo';
+import { Category } from './Category';
 import { Priority } from './Priority';
 
 // TODO: replace with DB info
-const TODOS = [new Todo(new Date(2017, 4, 30), false, undefined, Priority.Low),
-                new Todo(new Date(2017, 4, 28), false, undefined, Priority.Medium),
-                new Todo(new Date(2017, 4, 29), true, new Date(2017, 4, 30), Priority.High)];
+const TODOS: Todo[] = [new Todo(new Category('Work', new Date(2017, 4, 30)), new Date(2017, 4, 30), false, undefined, Priority.Low),
+                new Todo(new Category('School', new Date(2017, 3, 26)), new Date(2017, 4, 28), false, undefined, Priority.Medium),
+                new Todo(null, new Date(2017, 4, 29), true, new Date(2017, 4, 30), Priority.High)];
 
 @Injectable()
 export class TodoService {
--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,7 +1,9 @@
+import { Category } from './Category';
 import { Priority } from './Priority';
 
 export class Todo {
-    constructor(public DateCreated: Date, 
+    constructor(public Category: Category,
+                public DateCreated: Date, 
                 public Done: boolean, 
                 public DateDone: Date,
                 public Priority: Priority){
