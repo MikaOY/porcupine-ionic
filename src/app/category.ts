cc4b7698b686c6d08f1cb774d7363e1f233caa7e
Mika Yeap <mikaomarr@gmail.com>
2017-05-04 16:48:57 +0800

    Added mock category retrieval service

--- src/app/category.ts
+++ src/app/category.ts
@@ -2,7 +2,7 @@ import { Priority } from './Priority';
 
 export class Category {
     constructor(public Name: string, 
-                public Color: string,
+                public Color: number, // select from a limited array
                 public DateCreated: Date, 
                 public Order?: number, 
                 public DefaultPriority?: Priority){
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -5,14 +5,20 @@ import { Category } from './Category';
 import { Priority } from './Priority';
 
 // TODO: replace with DB info
-const TODOS: Todo[] = [new Todo('Give an alpaca a hug', new Category('Life', 'red', new Date(2017, 4, 30)), new Date(2017, 4, 30), false, undefined, Priority.Low),
-                        new Todo('Finish Porcupine', new Category('Code', 'grey', new Date(2017, 3, 26)), new Date(2017, 4, 28), false, undefined, Priority.Medium),
+const TODOS: Todo[] = [new Todo('Give an alpaca a hug', new Category('Life', 0, new Date(2017, 4, 30)), new Date(2017, 4, 30), false, undefined, Priority.Low),
+                        new Todo('Finish Porcupine', new Category('Code', 1, new Date(2017, 3, 26)), new Date(2017, 4, 28), false, undefined, Priority.Medium),
                         new Todo('Make moist brownie', null, new Date(2017, 4, 29), true, new Date(2017, 4, 30), Priority.High)];
+
+const CATS: Category[] = [new Category('Life', 0, new Date(2017, 4, 30)), new Category('Code', 1, new Date(2017, 3, 26)),];
 
 @Injectable()
 export class TodoService {
 
     getTodos(): Promise<Todo[]> {
         return Promise.resolve(TODOS);
+    }
+
+    getCategories(): Promise<Category[]> {
+        return Promise.resolve(CATS);
     }
 }
