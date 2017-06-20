ba36c02727b69b7b32c872c3758518e9a0323d6f
Mika Yeap <mikaomarr@gmail.com>
2017-06-20 19:58:37 +0800

    Fixed formatting sharing modal files

--- src/app/filter/board-manager/share-page/share-page.component.ts
+++ src/app/filter/board-manager/share-page/share-page.component.ts
@@ -1,60 +1,61 @@
-import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
-import { Board } from '../../../board';
-import { Recipient } from '../../../recipient';
-import { ViewController } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Board } from '../../../board';
+import { Recipient } from '../../../recipient';
+import { ViewController } from 'ionic-angular';
 
 @Component({
-  templateUrl: 'share-page.html',
+	templateUrl: 'share-page.html',
 })
 
-export class SharePage {
-  constructor(public navParams: NavParams,
-              public viewCntrl: ViewController) { }
-  sharees: Recipient[] = [];
-  note: string = "Check this out!";
-  containsEdit: boolean = false;
-  containsViewOnly: boolean = false;
+export class SharePage {
+	constructor(public navParams: NavParams,
+		public viewCntrl: ViewController) { }
 
-  shareBoard() {
-    //send to service
-    var sBoard: Board = this.navParams.get("sBoard");
-    if (this.sharees.length == 0){
-      this.sharees.push(new Recipient("skanklyone@gmail.com", true));
-    }
-    console.log("sharing board:" + sBoard.Name + " with " + this.sharees.length + " people with note: " + this.note);
-    this.viewCntrl.dismiss();
-  }
+	sharees: Recipient[] = [];
+	note: string = "Check this out!";
+	containsEdit: boolean = false;
+	containsViewOnly: boolean = false;
+	newReci: Recipient = new Recipient(undefined, false);
 
-  newReci: Recipient = new Recipient(undefined, false);
-  addReci(reci: Recipient){
-    this.sharees.push(reci);
-    if (this.containsEdit == false || this.containsViewOnly== false){
-      if (reci.ViewOnly == true){
-        this.containsViewOnly= true;
-      }
-      if (reci.ViewOnly == false){
-        this.containsEdit = true;
-      }
-    }
-    this.newReci= new Recipient(undefined, false);
-  }
+	shareBoard() {
+		//send to service
+		var sBoard: Board = this.navParams.get("sBoard");
+		if (this.sharees.length == 0) {
+			this.sharees.push(new Recipient("skanklyone@gmail.com", true));
+		}
+		console.log("sharing board:" + sBoard.Name + " with " + this.sharees.length + " people with note: " + this.note);
+		this.viewCntrl.dismiss();
+	}
 
-  removeReci(reci: Recipient){
-    this.sharees.splice(this.sharees.indexOf(reci), 1);
+	addReci(reci: Recipient) {
+		this.sharees.push(reci);
+		if (this.containsEdit == false || this.containsViewOnly == false) {
+			if (reci.ViewOnly == true) {
+				this.containsViewOnly = true;
+			}
+			if (reci.ViewOnly == false) {
+				this.containsEdit = true;
+			}
+		}
+		this.newReci = new Recipient(undefined, false);
+	}
+
+	removeReci(reci: Recipient) {
+		this.sharees.splice(this.sharees.indexOf(reci), 1);
 		this.containsViewOnly = this.doesContainViewOnly();
-  }
+	}
 
 	private doesContainViewOnly(): boolean {
-		for (let reci of this.sharees) {
-      if (reci.ViewOnly == true) {
-        return true;
-      }
-    }
+		for (let reci of this.sharees) {
+			if (reci.ViewOnly == true) {
+				return true;
+			}
+		}
 		return false;
 	}
 
-  closeModal(){
-    this.viewCntrl.dismiss();
-  }
+	closeModal() {
+		this.viewCntrl.dismiss();
+	}
 }
