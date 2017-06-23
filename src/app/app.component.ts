0773d75edb94219c4c6c37755db98f726e008565
omnikitty <omnikitty.powers@gmail.com>
2017-06-23 12:09:50 +0800

    added refresh token functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Platform, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from './auth.service';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from './login/login.component';
@@ -22,8 +23,10 @@ export class MyApp implements OnInit {
 	constructor(platform: Platform,
 							statusBar: StatusBar,
 							splashScreen: SplashScreen,
+							public modalCtrl: ModalController,
+							public auth: AuthService,
 							private settingsService: SettingsService,
-							modalCtrl: ModalController) {
+							) {
 		platform.ready().then((source) => {
 			// Okay, so the platform is ready and our plugins are available.
 			// Here you can do any higher level native things you might need.
@@ -31,7 +34,7 @@ export class MyApp implements OnInit {
 				statusBar.styleDefault();
 				splashScreen.hide();
 			}
-
+			this.auth.startupTokenRefresh();
 			let loginModal = modalCtrl.create(LoginPage);
 			loginModal.present();
 		});
