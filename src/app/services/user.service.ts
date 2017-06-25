8dafaff0810e7c87c798d68e6c5c23620b3cff86
Mika Yeap <mikaomarr@gmail.com>
2017-06-25 15:53:47 +0800

    Merging from cleanup

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,100 +5,150 @@ import { AuthHttp, JwtHelper } from 'angular2-jwt';
 
 import { User } from '../classes/user';
 
+//import * as bcrypt from '../../node_modules/bcrypt';
+
 declare var Auth0Lock: any;
 
 @Injectable()
 export class UserService {
-	clientId: string = 'pBum20Ve6T5n76t05t6tue5G2MMk9I3d'
-	auth0Domain: string = 'porcupine.au.auth0.com'
-  jwtHelper: JwtHelper = new JwtHelper();
-  lock = new Auth0Lock(this.clientId, this.auth0Domain);
-  local: Storage = new Storage(localStorage);
-  user: Object;
+	private clientId: string = 'pBum20Ve6T5n76t05t6tue5G2MMk9I3d'
+	private auth0Domain: string = 'porcupine.au.auth0.com'
+	private lock = new Auth0Lock(this.clientId, this.auth0Domain);	
+	private userAuthO: Object;	
+	private userDb: User;
+	private jwtHelper: JwtHelper = new JwtHelper();
+	private local: Storage = new Storage(localStorage);
 
-	//TODO: make this unnecessary
+	// TODO: make this unnecessary
 	isAuthenticated: boolean = false;
-
 	refreshSubscription: any;
 
-	public currentUserId: number;
+	// password hashing
+	private saltRounds = 10;
+
+	// TODO: migrate to constants file
 	private apiUrl: string = 'http://porcupine-dope-api.azurewebsites.net';
-	// private headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+	private headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
 	// private options = new RequestOptions({ headers: this.headers });
 
-	constructor(private http: Http,
-							private authHttp: AuthHttp) { 
+	constructor(private http: Http, private authHttp: AuthHttp) {
+		// get local user profile
 		this.local.get('profile').then(profile => {
-      this.user = JSON.parse(profile);
-    }).catch(error => {
-      console.log(error);
-    });
+			this.userAuthO = JSON.parse(profile);
+		}).catch(error => {
+			console.log(error);
+		});
 
-		this.lock.on('authenticated', authResult => {
-      this.lock.getProfile(authResult.idToken, (error, profile) => {
-        if (error) {
-          alert(error);
-          return;
-        }
+		// event handler for successful login
+		this.lock.on("authenticated", authResult => {
+			this.lock.getProfile(authResult.idToken, (error, profile) => {
+				if (error) {
+					alert(error);
+					return;
+				}
 
-        this.local.set('id_token', authResult.idToken);
-        this.local.set('profile', JSON.stringify(profile));
-        this.user = profile;
+				this.local.set('id_token', authResult.idToken);
+				this.local.set('profile', JSON.stringify(profile));
+				this.userAuthO = profile;
 				console.log('do this run');
-      });
-    });
+			});
+		});
+
+		// this.setPassword('1234'); 
 	}
 
-	public authenticated(): boolean {
-    // Check if there's an unexpired JWT
-    //return tokenNotExpired(); //TODO: tokenNotExpired fix
-		return this.isAuthenticated;
-  }
-
-  public login() {
-    // Show the Auth0 Lock widget
-    this.lock.show({
-      authParams: {
-        scope: 'openid offline_access',
-        device: 'Mobile device'
-      }
-    }, (err, profile, token, accessToken, state, refreshToken) => {
-      if (err) {
-        alert(err);
-      }
-      // If authentication is successful, save the items
-      // in local storage
-      this.local.set('profile', JSON.stringify(profile));
-      this.local.set('id_token', token);
-      this.local.set('refresh_token', refreshToken);
-      this.user = profile;
+	public login() {
+		// Show the Auth0 Lock widget
+		this.lock.show({
+			authParams: {
+				scope: 'openid offline_access',
+				device: 'Mobile device'
+			}
+		}, (err, profile, token, accessToken, state, refreshToken) => {
+			if (err) {
+				alert(err);
+			}
+			// If authentication is successful, save the items
+			// in local storage
+			this.local.set('profile', JSON.stringify(profile));
+			this.local.set('id_token', token);
+			this.local.set('refresh_token', refreshToken);
+			this.userAuthO = profile;
 			this.isAuthenticated = true;
-			console.log('It logged in???');
-			console.log('is authenticated: ' + this.isAuthenticated);
-    });    
-  }
 
-  public logout() {
-    this.local.remove('profile');
-    this.local.remove('id_token');
-    this.local.remove('refresh_token');
-    this.user = null;
-  }
+			console.log("User authenticated: " + this.isAuthenticated);
 
-
-/*USER.SERVICE functions*/
-	getUser(): Promise<number> { //TODO: should get currentUser
-		var user: number;
-		if (this.currentUserId != undefined) {
-			user = this.currentUserId;
-			console.log("GET: getUser user value: " + user);
-		}
-		else {
-			console.log("GET: Please Login!!");
-			user = undefined;
-		}
-		return Promise.resolve(user);
+			// TODO: get DB user, GET app data
+		});
 	}
+
+	public logout() {
+		this.local.remove('profile');
+		this.local.remove('id_token');
+		this.local.remove('refresh_token');
+		this.userAuthO = null;
+	}
+
+	public checkIfAuthenticated(): boolean {
+		// Check if there's an unexpired JWT
+		// return tokenNotExpired(); //TODO: tokenNotExpired fix
+		return this.isAuthenticated;
+	}
+
+	getUser(authOId?: string, forceGet?: boolean): Promise<User> {
+		if (this.userDb == undefined || (forceGet != undefined && forceGet == true)) {
+			if (authOId == undefined) {
+				Promise.reject('AuthO id not given to getUser method!');
+			} else {
+				return this.getUserById(authOId).then((user) => {
+					this.userDb = user;
+					return user;
+				});
+			}
+		} else {
+			return Promise.resolve(this.userDb);
+		}
+	}
+	/*
+	setPassword(plainP: string) {
+		let daHash;
+		bcrypt.hash(plainP, this.saltRounds, function (err, hash) {
+			daHash = hash;
+			console.log('Updating password!');
+
+			const url = `${this.apiUrl}/user`;
+
+			var details = {
+				'fname': this.userDb.fname,
+				'lname': this.userDb.lname,
+				'username': this.userDb.username,
+				'email': this.userDb.email,
+				'hash': hash,
+			};
+			let formBody = [];
+			for (var property in details) {
+				var encodedKey = encodeURIComponent(property);
+				var encodedValue = encodeURIComponent(details[property]);
+				formBody.push(encodedKey + '=' + '\'' + encodedValue + '\'');
+			}
+			let body = formBody.join('&');
+
+			return this.http.put(url, body, this.options).toPromise().then((response: any) => {
+				this.userDb.PasswordHash = daHash;
+				console.log(this.userDb.PasswordHash);
+				console.log('Update user response: ' + response.toString());
+			}).catch(this.handleError);
+		});
+	}
+
+	checkPassword(plainP: string): boolean {
+		let bool: boolean = false;
+		bcrypt.compare(plainP, this.userDb.PasswordHash, (err, res) => {
+			bool = res;
+		});
+		return bool;
+	}
+	*/
 
 	getUserByEmail(email: string): Promise<User> {
 		console.log('getting user by email');
@@ -114,16 +164,16 @@ export class UserService {
 		});
 	}
 
-	getUserById(id: number): Promise<User> {
+	getUserById(id: string): Promise<User> {
 		console.log('getting user by id');
 
-		const url = `${this.apiUrl}/user?id=${id}`;
+		const url = `${this.apiUrl}/user?authOId=${id}`;
 		return this.http.get(url).toPromise().then((response: any) => {
 			console.log('processing user by id');
 
 			let user: User = this.processIntoUser(response);
 
-			console.log('User by id retrieved!'); 
+			console.log('User by id retrieved!');
 			return user;
 		});
 	}
@@ -131,7 +181,7 @@ export class UserService {
 	private processIntoUser(response: any) {
 		let user: User;
 		for (let json of response.json()) {
-			user = new User(json['person_id'], json['fname'], json['lname'], json['username'], json['person_email']);
+			user = new User(json['person_id'], json['fname'], json['lname'], json['username'], json['person_email'], json['password_hash']);
 		}
 		return user;
 	}
