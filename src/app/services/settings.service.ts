8f5e9cf4baa6c42fa28519e7667aa30ceb7c5775
Mika Yeap <mikaomarr@gmail.com>
2017-06-25 16:30:20 +0800

    Added back settings.ts (accidental delete during conflict resolve)

---
+++ src/app/services/settings.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/Rx';
+
+import { UserService } from './user.service';
+
+@Injectable()
+export class SettingsService {
+	private theme: BehaviorSubject<string>;
+	availableThemes: { className: string, displayName: string }[];
+	passcode: string;
+
+	constructor(public userService: UserService) {
+		this.theme = new BehaviorSubject('null'); // theme default value
+		this.availableThemes = [
+			{ className: 'aqua-theme', displayName: 'Aqua' },
+			{ className: 'red-theme', displayName: 'Red' },
+			{ className: 'null', displayName: 'Default' }
+		];
+		this.passcode = '1234';
+	}
+
+	setTheme(val) {
+		this.theme.next(val);
+	}
+
+	getTheme() {
+		return this.theme.asObservable();
+	}
+
+	getPasscode(): Promise<string> {
+		var user;
+		this.userService.getUser().then(val => user = val);
+		//TODO: fetch this user's passcode and return it
+		return Promise.resolve(this.passcode);
+	}
+}
