0b8133bfcf4d1927c78465bf993ac833d1235d05
Mika Yeap <mikaomarr@gmail.com>
2017-06-28 21:33:45 +0800

    Updated to be same as ionic repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,51 +29,17 @@ describe('MyApp Component', () => {
     })
   }));
 
-  it('should be true', () => {
-    expect(true).toBe(true);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyApp);
+    component = fixture.componentInstance;
+  });
+
+  it('should be created', () => {
+    expect(component instanceof MyApp).toBe(true);
+  });
+
+  it('should have two pages', () => {
+    expect(component.pages.length).toBe(2);
   });
 
 });
-
-
-
-
-
-// import { async, TestBed } from '@angular/core/testing';
-// import { MyApp } from './app.component';
-// import { IonicModule, Platform } from 'ionic-angular';
-// import { StatusBar } from '@ionic-native/status-bar';
-// import { SplashScreen } from '@ionic-native/splash-screen';
-// import { PlatformMock, StatusBarMock, SplashScreenMock } from '../../test-config/mocks-ionic';
-
-// describe("A test suite", function () {
-// 	let fixture;
-// 	let component;
-
-// 	beforeEach(async(() => {
-// 		TestBed.configureTestingModule({
-// 			declarations: [MyApp],
-// 			imports: [
-// 				IonicModule.forRoot(MyApp)
-// 			],
-// 			providers: [
-// 				{ provide: StatusBar, useClass: StatusBarMock },
-// 				{ provide: SplashScreen, useClass: SplashScreenMock },
-// 				{ provide: Platform, useClass: PlatformMock }
-// 			]
-// 		})
-// 	}));
-
-// 	beforeEach(() => {
-// 		fixture = TestBed.createComponent(MyApp);
-// 		component = fixture.componentInstance;
-// 	});
-
-// 	it('should be created', () => {
-// 		expect(component instanceof MyApp).toBe(true);
-// 	});
-
-// 	it('should return -1 when the value is not present', function() {
-//       assert.equal(-1, [1,2,3].indexOf(4));
-//     });
-// }); 
