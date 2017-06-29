9f4f76c2509d7ca7e6c0778af672f0ab7ea7463f
omnikitty <omnikitty.powers@gmail.com>
2017-06-29 21:51:50 +0800

    Created shared module for classes and interfaces

---
+++ src/app/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+import { Board } from './classes/board';
+import { Todo } from './classes/todo';
+import { Category } from './classes/category';
+import { Permission } from './classes/permission';
+import { Priority } from './classes/priority';
+import { User } from './classes/user';
+import { DbCompatible } from './interfaces/db-compatible.interface';
+import { Lockable } from './interfaces/lockable.interface';
+
+@NgModule({
+	imports: [],
+	exports: [],
+	declarations: [],
+	providers: [],
+})
+export class SharedModule { }
