ff8bb9e7c23c55921b137581b2a793955b16f984
Mika Yeap <mikaomarr@gmail.com>
2017-06-29 22:07:34 +0800

    Done TodosModule

--- src/pages/todos/todos.module.ts
+++ src/pages/todos/todos.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { TodosPage } from './todos';
 import { TodoList } from "./todo-list/todo-list.component";
+import { SideMenuModule } from "../side-menu/side-menu.module";
+import { SharedModule } from "../../app/shared.module";
+import { CoreModule } from "../../app/services/core.module";
 
 @NgModule({
-	imports: [ /*side-menu, shared, core*/ ],
+	imports: [ SideMenuModule, SharedModule, CoreModule ],
 	exports: [ TodosPage, TodoList ],
 	declarations: [ TodosPage, TodoList ],
 	providers: [],
