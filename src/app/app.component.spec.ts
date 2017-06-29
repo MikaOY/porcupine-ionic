3715f1f4cfd44353da93b76a94a1b310460d380d
omnikitty <omnikitty.powers@gmail.com>
2017-06-29 10:20:25 +0800

    Testing todo.service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,11 +5,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MyApp } from './app.component';
+import { SideMenu } from '../pages/side-menu/side-menu.component';
+import { AppModule } from './app.module';
 import {
   PlatformMock,
   StatusBarMock,
   SplashScreenMock
 } from '../../test-config/mocks-ionic';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('MyApp Component', () => {
   let fixture;
@@ -17,22 +20,23 @@ describe('MyApp Component', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MyApp],
-      imports: [
-        IonicModule.forRoot(MyApp)
-      ],
+      declarations: [ MyApp ],
+			schemas: [ NO_ERRORS_SCHEMA ],
+      // imports: [
+      //   IonicModule.forRoot(MyApp)
+      // ],
       providers: [
         { provide: StatusBar, useClass: StatusBarMock },
         { provide: SplashScreen, useClass: SplashScreenMock },
         { provide: Platform, useClass: PlatformMock }
       ]
-    })
+    }).compileComponents();
   }));
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(MyApp);
-  //   component = fixture.componentInstance;
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyApp);
+    component = fixture.componentInstance;
+  });
 
   it('should be true', () => {
     expect(true).toBe(true);
--- src/pages/todos/todo-list/todo-list.spec.ts
+++ src/pages/todos/todo-list/todo-list.spec.ts
@@ -1,13 +1,63 @@
 
-//  import TodoList from './todo-list.component';
 //  import { Category } from '../../../app/classes/category';
 //  import { Todo } from '../../../app/classes/todo';
 //  import { Priority } from '../../../app/classes/priority';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { IonicModule, Platform } from 'ionic-angular';
+import { TodoList } from './todo-list.component';
 
-// var t: TodoList.TodoList = null;
-// var c: Category;
-// var todo: Todo;
-// describe('TodoList', () => {
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { TodoService } from '../../../app/services/todo.service';
+
+import { PlatformMock, StatusBarMock, SplashScreenMock } 
+	from '../../../../test-config/mocks-ionic';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+
+describe('TodoList Component', () => {
+  let fixture: ComponentFixture<TodoList>;
+  let component: TodoList;
+	let de: DebugElement;
+
+  beforeEach(async(() => {
+		var todoServiceStub = {
+			
+		}
+    TestBed.configureTestingModule({
+      declarations: [TodoList],
+			schemas: [ NO_ERRORS_SCHEMA ],
+      imports: [
+        IonicModule.forRoot(TodoList)
+      ],
+      providers: [
+				{ provide: TodoService, useClass: todoServiceStub},
+        { provide: StatusBar, useClass: StatusBarMock },
+        { provide: SplashScreen, useClass: SplashScreenMock },
+        { provide: Platform, useClass: PlatformMock }
+      ]
+    });
+  }));
+
+	beforeEach(() => {
+		
+    fixture = TestBed.createComponent(TodoList);
+    component = fixture.componentInstance;
+
+		var todoService = TestBed.get(TodoService);
+  });
+
+	// describe('#activateSelect()', () => {
+	// 	it('should make selectActive true', () => {
+	// 		component.activateSelect(this.testTodo);
+	// 		expect(component.isSelectActive).toBe(true);
+	// 	});
+	// });
+});
+
+  
 	
 // 	beforeEach(() => {
 // 		t = new TodoList.TodoList(undefined, undefined);
@@ -15,11 +65,8 @@
 // 		var testTodo = new Todo('Take out trash', testCat, new Date(), 
 // 			false, undefined, false, Priority.Low, 2);
 // 	});
-// 	describe('#activateSelect()', () => {
-// 		it('should make selectActive true', () => {
-// 			this.t.activateSelect(this.testTodo);
-// 			assert(this.t.isSelectActive == true, 'test fail');
-// 		});
-// 	});
+	
 // });
+
+
 
