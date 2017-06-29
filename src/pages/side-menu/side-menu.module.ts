2e5280dc2cae0375f28fa37ce3b0ee3fdd91abdf
omnikitty <omnikitty.powers@gmail.com>
2017-06-29 22:26:53 +0800

    Added ionic for root for side-menu module

--- src/pages/side-menu/side-menu.module.ts
+++ src/pages/side-menu/side-menu.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
 import { SideMenu } from './side-menu.component';
 import { ProfilePage } from './profile/profile.component';
@@ -6,9 +7,10 @@ import { BoardManager } from './board-manager/board-manager.component';
 import { UnlockPage } from './board-manager/unlock-page/unlock-page.component';
 import { SharePage } from './board-manager/share-page/share-page.component';
 import { AddCategory } from './add-category/add-category.component';
+import { MyApp } from '../../app/app.component';
 
 @NgModule({
-	imports: [],
+	imports: [ IonicModule.forRoot(MyApp) ],
 	exports: [ SideMenu ],
 	declarations: [
 		SideMenu,
