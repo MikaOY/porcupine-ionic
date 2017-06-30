0935737144ac7c7085e31c51a3a1ef831f4d9f23
omnikitty <omnikitty.powers@gmail.com>
2017-06-30 11:12:54 +0800

    Added setup for http testing

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,19 +1,18 @@
-import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { ComponentFixture, async, TestBed, inject } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod, ResponseContentType } from '@angular/http';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { ComponentFixtureAutoDetect } from '@angular/core/testing';
 
 import { IonicModule, Platform } from 'ionic-angular';
-import { TodoService } from '../../app/services/todo.service';
+import { UserService } from './user.service';
 import { Mocks, NavParamsMock } from '../../../test-config/mocks-ionic';
 
-describe('Todos page', () => {
-	let tServ;
-	let todoServiceStub = {
-		slothGetCurrentBoard() {
-			return Promise.resolve(Mocks.Board);
-		}
-	};
+describe('User service', () => {
+	let subject: UserService;
+  let backend: MockBackend;
+  let responseForm = '<form />';
 
 	// async beforeEach (to allow external templates to be compiled)
 	beforeEach(async(() => {
@@ -24,16 +23,46 @@ describe('Todos page', () => {
 			// providers:    [ UserService ]  
 			// Provide a test-double instead
 			providers: [
-				{ provide: TodoService, useValue: todoServiceStub }
+				UserService,
+				MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (mockBackend, defaultOptions) => {
+            return new Http(mockBackend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        }
 			]
 		})
 			// compile template and css
 			// NOTE: DO NOT config TestBed after method below
-			.compileComponents();
+			// .compileComponents();
 	}));
 
-	// synchronous beforeEach (waits for async beforeEach above to complete)
-	beforeEach(() => {
-		tServ = TestBed.get(TodoService);
-	});
+	beforeEach(inject([UserService, MockBackend], (userService, mockBackend) => {
+    subject = userService;
+    backend = mockBackend;
+  }));
+
+	it('should be called with proper arguments', (done) => {
+    backend.connections.subscribe((connection: MockConnection) => {
+      expect(connection.request.url).toEqual('https://blacksonic.eu.auth0.com.auth0.com/usernamepassword/login');
+      expect(connection.request.method).toEqual(RequestMethod.Post);
+      expect(connection.request.headers.get('Content-Type')).toEqual('application/json');
+      expect(connection.request.getBody()).toEqual(JSON.stringify(
+        {
+          username: 'blacksonic',
+          password: 'secret',
+          client_id: 'YOUR_CLIENT_ID'
+        }, null, 2
+      ));
+      // expect(connection.request.detectContentType()).toEqual(ResponseContentType.Json);
+
+      let options = new ResponseOptions({
+        body: responseForm
+      });
+
+      connection.mockRespond(new Response(options));
+    });
 });
