c682ed93c05f61c88bfff8befbbc235c779541b7
omnikitty <omnikitty.powers@gmail.com>
2017-07-06 10:21:03 +0800

    Add todo button removed and moved to top of todo list
    Details show up on input focus

--- src/pages/todos/todos.module.ts
+++ src/pages/todos/todos.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from "ionic-angular";
 
-import { MyApp } from "../../app/app.component";
+import { MyApp } from '../../app/app.component';
 import { TodosPage } from './todos';
-import { TodoList } from "./todo-list/todo-list.component";
-import { SideMenuModule } from "../side-menu/side-menu.module";
-import { SharedModule } from "../../app/shared.module";
-import { CoreModule } from "../../app/services/core.module";
+import { TodoList } from './todo-list/todo-list.component';
+import { CatList } from './cat-list/cat-list.component';
+import { SideMenuModule } from '../side-menu/side-menu.module';
+import { SharedModule } from '../../app/shared.module';
+import { CoreModule } from '../../app/services/core.module';
 
 @NgModule({
 	imports: [IonicModule.forRoot(MyApp), SideMenuModule, SharedModule, CoreModule],
 	exports: [TodosPage, TodoList],
-	declarations: [TodosPage, TodoList],
+	declarations: [TodosPage, TodoList, CatList],
 	providers: [],
 	entryComponents: [ TodosPage ],
 })
